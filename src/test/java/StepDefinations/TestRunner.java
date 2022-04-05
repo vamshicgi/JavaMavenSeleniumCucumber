d30a781f56cb20ff9de51ead93b77295811de178
CGIVAMSHI <cgi.vamshi@gmail.com>
2022-04-05 13:01:23 +0530

    Saving Test Report in Target folder in html format

--- src/test/java/StepDefinations/TestRunner.java
+++ src/test/java/StepDefinations/TestRunner.java
@@ -5,7 +5,8 @@ import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features="src/test/resources/Features",glue={"StepDefinations"},monochrome = true)
+@CucumberOptions(features="src/test/resources/Features",glue={"StepDefinations"},monochrome = true,
+plugin = {"pretty","html:target/HtmlReports"})
 public class TestRunner {
 
 }
